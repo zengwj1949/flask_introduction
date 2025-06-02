c6354fe80077031108c22cb301866acc0ca94076
dev_zwj <2962372861@qq.com>
2025-06-02 17:41:57 +0800

    add env

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+# -*- coding: utf-8 -*-
+
 # 一、导入模块
 from flask import Flask
+import os
+
 
 # 二、创建 Flask 实例
 app = Flask(__name__)
+app.config.from_pyfile('conf/config.py', silent=False)
 
 # 三、定义路由及视图函数
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def hello():
-    return 'Welcome to My Watchlist!'
+    return 'Welcome to My Watchlist!!!'
 
 # 四、运行项目
+if __name__ == "__main__":
+	app.run()
+
---
+++ conf/config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+SERVER_NAME = "127.0.0.1:9000"
+DEBUG = True
+
